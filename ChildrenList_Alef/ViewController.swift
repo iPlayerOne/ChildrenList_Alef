d4d3f5333bfdaef840b86fb410f18dbf1b325f60
Ivan Korobov <ikorobov.dev@gmail.com>
2025-02-17 16:25:39 +0100

    Initial Commit

---
+++ ChildrenList_Alef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChildrenList_Alef
+//
+//  Created by ikorobov on 17.2.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
